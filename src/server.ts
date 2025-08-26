0783af12b6c8f092bd116a07e22c2bc67b2e25e7
Endrius <endriussilvasantos@gmail.com>
2025-08-25 21:31:14 -0300

    adicionando correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,16 @@ app.get("/movies", async (_, res) => {
 app.post("/movies", async (req, res) => {
   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
+  const movieWithSameTitle = await prisma.movie.findFirst({
+    where: { title: { equals: title, mode: "insensitive" } },
+  });
+
+  if (movieWithSameTitle) {
+    return res
+      .status(409)
+      .send({ message: "Já existe um filme cadastrado com esse título" });
+  }
+
   try {
     await prisma.movie.create({
       data: {
